49592fab62b75ea5e268b6090d1365f0c45975d7
Vishal Anand <online_vishalanand@yahoo.com>
2016-06-20 18:06:27 +0530

    v.0.0.1

---
+++ spark/src/main/scala/org/test/spark/SparkTest.scala
@@ -0,0 +1,21 @@
+
+
+package org.test.spark
+
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkConf
+
+object SparkTest {
+      def main(args: Array[String]) {
+      val conf=new SparkConf()
+      .setAppName("SparkTest")
+      .setMaster("local")
+      val sc=new SparkContext(conf)
+      val tf=sc.textFile("file:///spark-1.6.1/README.md")
+      val tokens=tf.flatMap(l=>l.split(" "))
+      val count=tokens.map(w=>(w,1))
+      val c_pair=tokens.map(w=>(w,1))
+      val counts=c_pair.reduceByKey((c,k)=>c+k)
+      val s_counts=counts.sortBy(p=>p._2,false).saveAsTextFile("count_20160620_2")
+    }
+}
